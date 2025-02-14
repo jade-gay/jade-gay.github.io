898cbb79008ad3d92f1ce665e9e60a456c7d9378
jade ⚘ <133483645+jade-gay@users.noreply.github.com>
2025-02-14 16:57:14 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,20 +95,20 @@ const updateSearchIndicator = (value) => {
       if (normalizedValue.includes(':')) {
         searchBadge.style.display = 'inline-block';
         searchBadge.textContent = `Searching subreddit`;
-        searchBadge.style.backgroundColor = '#ea9a97';
+        searchBadge.style.backgroundColor = '#F07489';
       } else {
         searchBadge.style.display = 'inline-block';
         searchBadge.textContent = `Searching Reddit`;
-        searchBadge.style.backgroundColor = '#ea9a97';
+        searchBadge.style.backgroundColor = '#F07489';
       }
     } else if (prefix === 'arch') {
       searchBadge.style.display = 'inline-block';
       searchBadge.textContent = `Searching Arch`;
-      searchBadge.style.backgroundColor = '#ea9a97'
+      searchBadge.style.backgroundColor = '#F07489'
     } else if (lookup[prefix] || engineUrls[prefix]) {
       searchBadge.style.display = 'inline-block';
       searchBadge.textContent = `Searching ${prefix}`;
-      searchBadge.style.backgroundColor = '#ea9a97';
+      searchBadge.style.backgroundColor = '#F07489';
     } else {
       searchBadge.style.display = 'none';
     }
